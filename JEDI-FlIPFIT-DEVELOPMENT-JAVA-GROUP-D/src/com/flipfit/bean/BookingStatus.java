8d2e67ad9445c7da7e8be4786a25572ecfd8d4b1
Amisha2004 <amisha_2101ee07@iitp.ac.in>
2025-07-06 13:33:49 +0530

    Commit

--- JEDI-FlIPFIT-DEVELOPMENT-JAVA-GROUP-D/src/com/flipfit/bean/BookingStatus.java
+++ JEDI-FlIPFIT-DEVELOPMENT-JAVA-GROUP-D/src/com/flipfit/bean/BookingStatus.java
@@ -1,7 +1,13 @@
 package com.flipfit.bean;
 
+/**
+ * Defines the possible states of a booking.
+ */
 public enum BookingStatus {
+   /** Booking is confirmed. */
    CONFIRMED,
+   /** Booking has been cancelled. */
    CANCELLED,
+   /** Booking is on a waitlist. */
    WAITLISTED
 }
