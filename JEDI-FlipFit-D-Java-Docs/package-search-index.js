a906ff106f0913206ae559e19fb32c6295ff9d79
Amisha2004 <amisha_2101ee07@iitp.ac.in>
2025-07-07 00:26:28 +0530

    Added Java docs

---
+++ JEDI-FlipFit-D-Java-Docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.flipfit.bean"},{"l":"com.flipfit.business"},{"l":"com.flipfit.client"},{"l":"com.flipfit.constants"},{"l":"com.flipfit.dao"},{"l":"com.flipfit.exceptions"}];updateSearchResults();
